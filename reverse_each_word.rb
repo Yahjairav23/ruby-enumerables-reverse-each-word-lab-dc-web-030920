139e9a1e82440b59cd6619b90758a43da89b3875
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 20:21:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def reverse_each_word(sentance)
   # binding.pry
-  yield(sentance)
+  yield(sentance.reverse)
 end
 
-sentance.each do
-  p "#{sentance.reverse}"
+sentance.each do |s|
+  return "#{s}"
 end

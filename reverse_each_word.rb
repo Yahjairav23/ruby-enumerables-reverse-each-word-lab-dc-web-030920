a44f92d1d4de17e8ce723e93826cf33d00785197
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-06 20:07:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 def reverse_each_word(string)
+  binding.pry
   yield(string)
 end
 
-reverse_each_word(string).each do |sentance|
-  puts "#{sentance.reverse}"
-end
+# reverse_each_word(string).each do |sentance|
+#   puts "#{sentance.reverse}"
+# end
